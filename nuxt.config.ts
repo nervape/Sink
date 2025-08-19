0900768a9c1222e3df33391bb7d4f95cd0ba0b21
Code Monad <code@lab-11.org>
2025-08-19 09:05:53 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,7 +41,7 @@ export default defineNuxtConfig({
 
   routeRules: {
     '/': {
-      predirect: '/dashboard', 
+      redirect: '/dashboard', 
     },
     '/dashboard/**': {
       prerender: true,
